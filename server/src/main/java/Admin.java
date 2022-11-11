eed6e42ee16f42ab745a1ba85e69e12fcf343d00
Michael Zhao <michaelzhao314@gmail.com>
2022-11-10 19:19:09 -0600

    Added admin and fixed Coach to extend User

---
+++ server/src/main/java/Admin.java
@@ -0,0 +1,5 @@
+public class Admin extends User {
+    public Admin(String username, String password, String fullName) {
+        super(username, password, fullName);
+    }
+}
--- server/src/main/java/Coach.java
+++ server/src/main/java/Coach.java
@@ -1,11 +1,16 @@
 import java.util.List;
 
-public class Coach {
+public class Coach extends User {
     private String residentGym;
     private String mainContact;
     private List<Customer> managedCustomers;
 
-    public Coach(String residentGym, String mainContact) {
+    public Coach(String username, String password, String fullName) {
+        super(username, password, fullName);
+    }
+
+    public Coach(String username, String password, String fullName, String residentGym, String mainContact) {
+        super(username, password, fullName);
         this.residentGym = residentGym;
         this.mainContact = mainContact;
     }
